5786bc2020c08be5e04ba8ef90cf7243ad106621
waleokare <waleokare@outlook.com>
2016-05-04 10:12:41 +0100

    access lab

--- Task3/index.php
+++ Task3/index.php
@@ -20,11 +20,11 @@
 
         <nav>
             <ul>
-                <li><a href="blog.html"> All Blog Items </a></li>
-                <li><a href="blog.html"> Work Items  </a></li>
-                <li><a href="blog.html">  University Items </a></li>
-                <li><a href="blog.html"> Family Items </a></li>
-                <li><a href="add.html"> Insert a Blog Item </a></li>
+                <li><a href="blog.php"> All Blog Items </a></li>
+                <li><a href="blog.php"> Work Items  </a></li>
+                <li><a href="blog.php">  University Items </a></li>
+                <li><a href="blog.php"> Family Items </a></li>
+                <li><a href="add.php"> Insert a Blog Item </a></li>
 
             </ul>
         </nav>
