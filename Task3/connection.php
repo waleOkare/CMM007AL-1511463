359e1a61ef8784fafc436e8427dcda907c8ddf80
waleokare <waleokare@outlook.com>
2016-05-04 10:49:32 +0100

    access lab

--- Task3/connection.php
+++ Task3/connection.php
@@ -14,7 +14,7 @@ $db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
 
 if ($db) {
 
-    echo "Successful Connection to the database <br>"  ;
+  //  echo "Successful Connection to the database <br>"  ;
 } else {
 
     echo "Failed to connect to the database";
